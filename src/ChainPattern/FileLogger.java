46fecef09a20bf884e5f006c84143ecab87e2a14
Jack <289199610@qq.com>
2023-08-05 10:31:38 +0800

    Chain Pattern

---
+++ src/ChainPattern/FileLogger.java
@@ -0,0 +1,12 @@
+package ChainPattern;
+
+public class FileLogger extends AbstractLogger {
+    public FileLogger(AbstractLogger nextLogger) {
+        setNextLogger(nextLogger);
+        level = INFO;
+    }
+
+    protected void write(String msg) {
+        System.out.println("INFO: " + msg);
+    }
+}
